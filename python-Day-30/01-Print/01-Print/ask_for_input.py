a6cb3531a33055551e4df61a5488afdf7cf15651
ᏤIᎦHᏘLᎽᏘᎠᏘᏤ <132227049+VISHAL291137@users.noreply.github.com>
2025-02-22 17:41:12 +0530

    Add files via upload

---
+++ python-Day-30/01-Print/01-Print/ask_for_input.py
@@ -0,0 +1,5 @@
+#The input funcition allows you to prompt the user for a value
+#yo need to declare a variable to hold the value the enterwd bu the user
+name = input("What is your name? ")
+
+print(name)
